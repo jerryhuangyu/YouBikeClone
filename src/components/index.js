c6724e626722b25748ed997f54a9157f226a8baf
jerryhuangyu <jerry0932619628@gmail.com>
2023-09-20 11:00:16 +0800

    update: Table section relative

--- src/components/index.js
+++ src/components/index.js
@@ -1,9 +1,11 @@
 import Header from "./header/Header";
 import SearchSection from "./search/SearchSection";
 import Border from './Border';
+import TableSection from "./table/TableSection";
 
 export {
     Header,
     SearchSection,
     Border,
+    TableSection,
 }
