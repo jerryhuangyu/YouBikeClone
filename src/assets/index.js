ff912baa801430afda0c0ab1ed189772f6f5c148
jerryhuangyu <jerry0932619628@gmail.com>
2023-09-20 10:57:20 +0800

    add: New assets

--- src/assets/index.js
+++ src/assets/index.js
@@ -1,5 +1,11 @@
 import biking from './biking.png'
+import downArrow from './downArrow.svg'
+import upArrow from './upArrow.svg'
+import twoWayArrow from './twoWayArrow.svg'
 
 export {
     biking,
+    downArrow,
+    upArrow,
+    twoWayArrow,
 }
